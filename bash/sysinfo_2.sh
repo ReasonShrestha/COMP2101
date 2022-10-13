6ed9f684095c7cf50c4439bdb3a20e1aab404f9c
ReasonShrestha <shresthareason2i2@gmail.com>
2022-10-13 11:01:04 -0400

    lab 2

---
+++ bash/sysinfo_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# sysinfo.sh - script that display report about the computer
+# hostname of device
+HOSTNAME=pc200500707
+# domain of device
+FQDN=(none)
+# ip address of device
+IP_ADDRESS=192.168.2.126/24
+# Free space available
+FREE_DISK_SPACE=7.5G
+#Printing
+=======================
+FQDN: 
+Operating System name and version: 
+IP ADDRESS: 
+Root FIlesystem free space: 
+=======================
