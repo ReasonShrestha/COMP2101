9fda0295ff9aef0c5896d69486845a8608ad30e0
Reason Shrestha <100056114+ReasonShrestha@users.noreply.github.com>
2022-12-04 13:43:50 -0500

    Delete LAB3.ps1

--- powershell/LAB3.ps1
+++
@@ -1,3 +0,0 @@
- get-ciminstance  win32_networkadapterconfiguration |
- ?{ $_.ipenabled -eq "True"} |
-Format-table -AutoSize Description, index, IPAddress, IPSubnet, DnsDomain, DNSSErverSearchOrder
