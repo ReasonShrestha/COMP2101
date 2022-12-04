93ae6837fb9a389ab747161ae483ffb7731ce80a
ReasonShrestha <Shresthareason2i2@gmail.com>
2022-12-04 13:34:45 -0500

    Lab3

---
+++ powershell/LAB3.ps1
@@ -0,0 +1,3 @@
+ get-ciminstance  win32_networkadapterconfiguration |
+ ?{ $_.ipenabled -eq "True"} |
+Format-table -AutoSize Description, index, IPAddress, IPSubnet, DnsDomain, DNSSErverSearchOrder
